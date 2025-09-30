0c151cba1e4c335ffe191c6438c7b83e6cfc3089
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-09-30 12:22:48 +0200

    removed warning

--- pm4py/statistics/overlap/__init__.py
+++ pm4py/statistics/overlap/__init__.py
@@ -1,7 +1,1 @@
 from pm4py.statistics.overlap import cases, utils, interval_events
-
-from pm4py.util import constants
-import warnings
-
-if constants.SHOW_INTERNAL_WARNINGS:
-    warnings.warn("The overlap package will be removed in a future release.")
