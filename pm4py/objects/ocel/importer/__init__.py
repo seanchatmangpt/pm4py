070797c16b67d7fb65cfa1af49285f29081ed33f
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2026-06-17 11:05:37 +0200

    OCEL 2.0 bundled importer/exporter

--- pm4py/objects/ocel/importer/__init__.py
+++ pm4py/objects/ocel/importer/__init__.py
@@ -1,4 +1,4 @@
-from pm4py.objects.ocel.importer import csv, jsonocel
+from pm4py.objects.ocel.importer import bundled, csv, jsonocel
 import importlib.util
 
 if importlib.util.find_spec("lxml"):
---
+++ pm4py/objects/ocel/importer/bundled/variants/__init__.py
@@ -0,0 +1,1 @@
+from pm4py.objects.ocel.importer.bundled.variants import ocel20
