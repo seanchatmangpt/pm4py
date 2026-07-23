c82619803dcf02af9b125e1f0de3d3399e574209
Alessandro Berti <alessandro.berti89@gmail.com>
2026-07-23 13:25:10 +0200

    first commit

--- pm4py/algo/conformance/alignments/petri_net/variants/__init__.py
+++ pm4py/algo/conformance/alignments/petri_net/variants/__init__.py
@@ -1,6 +1,7 @@
 from pm4py.algo.conformance.alignments.petri_net.variants import (
     dijkstra_less_memory,
     dijkstra_no_heuristics,
+    dijkstra_semantics,
     state_equation_a_star,
     discounted_a_star,
     approx_tandem_repeats,
